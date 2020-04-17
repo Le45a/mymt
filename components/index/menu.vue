7a56682e31cdabccb8751280ceaba4f6357b694e
le <2549720891@qq.com>
2020-04-17 21:11:16 +0800

    ““头部完成”

---
+++ components/index/menu.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="m-menu">
+    <dl class="nav">
+      <dt>全部分类</dt>
+      <dd v-for="(item,idx) in menu" :key="idx">
+        <i :class="item.type" />{{ item.name }}<span class="arrow" />
+      </dd>
+    </dl>
+    <div class="detail">
+      <template>
+        <h4>标题</h4>
+        <span>子项</span>
+      </template>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      menu: [{
+        type: 'food',
+        name: '美食'
+      }, {
+        type: 'takeout',
+        name: '外卖'
+      }]
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ components/public/header/geo.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="m-geo">
+    <i class="el-icon-location">北京市</i>
+    <nuxt-link class="changeCity" to="/changeCity">
+      切换城市
+    </nuxt-link>
+    [天津，深圳，廊坊]
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+</style>
